7b7f338fbfb6c7e4a8d42d49d6e72547e2b4d2fb
Eduardo Nabha <102608930+EduardoNabha@users.noreply.github.com>
2022-06-08 10:45:38 -0300

    Update class.py

--- class.py
+++ class.py
@@ -1,1 +1,13 @@
+class Aluno:
+    def __init__(self,ra='',digito='',nome='',cidadeNascimento='',dtNascimento='',ufNascimento=''):
+        self.ra=ra
+        self.digito=digito
+        self.nome=nome
+        self.cidadeNascimento=cidadeNascimento
+        self.dtNascimento=dtNascimento
+        self.ufNascimento=ufNascimento
 
+    def getRaDigito(self):
+        self.ra=input('Insíra o RA:\n')
+        self.digito=input('Insíra o Dígito do RA:\n')
+        
